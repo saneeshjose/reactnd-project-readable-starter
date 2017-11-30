fde81fc3cbcb26c85f1de4941f0843299d6a01de
Joseph, Saneesh <Saneesh_Joseph@intuit.com>
2017-11-30 07:07:40 -0800

    Sort functionality

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -1,16 +1,18 @@
  import React, {Component} from 'react';
+ import Moment from 'react-moment';
 
  class Post extends Component {
 
  	render() {
 
- 		const {title,body,author,category,commentCount,voteScore} = this.props;
+ 		const {title,body,author,category,commentCount,voteScore,timestamp} = this.props;
 
  		return <div className="post">
  			<div className="post-title">{title}</div>
  			<div className="post-body">{body}</div>
  			<div className="post-attributes">
 	 			<span className="post-attribute">Posted By: {author}</span>
+	 			<span className="post-attribute">On: <Moment format="MM/DD/YY HH:MM">{timestamp}</Moment></span>
 	 			<span className="post-attribute">Category: {category}</span>
 	 			<span className="post-attribute">Comments: {commentCount}</span>
 	 			<span className="post-attribute">Votes: {voteScore}</span>
--- frontend/src/Posts.js
+++ frontend/src/Posts.js
@@ -6,14 +6,34 @@ import Post from './Post';
 
 class Posts extends Component{
 
+	state = {
+		sortOrder : 'desc',
+		sortBy : 'voteScore'
+	}
+
+	changeSort = (e)=> {
+		this.setState( {sortBy : e.target.value} );
+	}
+
 	render() {
 
 		console.log('Rendering Posts : ' + this.props.selectedCategory );
 
+
 		const {posts, selectedCategory} = this.props;
 		const filteredPosts = selectedCategory === 'all' ? posts : posts.filter((p)=>p.category === selectedCategory );
 
+		filteredPosts.sort((p1,p2)=>p1[this.state.sortBy]-p2[this.state.sortBy]);
+		this.state.sortOrder === 'desc' && filteredPosts.reverse();
+
 		return <div className="posts">
+			<div className="sort-panel">
+				<span>Sort By</span>
+				<select onChange={this.changeSort}>
+					<option value="voteScore">Votes</option>
+					<option value="timestamp">Date</option>
+				</select>
+			</div>
 			{filteredPosts.map((p)=><Post key={p.id} {...p}/>)}
 		</div>
 	}
